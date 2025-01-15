df3b92e2c13383b9fd955c7f380c9bb1b9050199
Pedro de Castro Kurtz <pedrokurtz.01@outlook.com>
2025-01-15 14:36:26 -0300

    back-end finalizado

--- scripts/teste.js
+++ scripts/teste.js
@@ -1,5 +1,5 @@
 (async function () {
-    const fileUrl = 'BaseDados.xlsx'; // Caminho do arquivo na mesma pasta do HTML
+    const fileUrl = 'RELATORIO.xlsx'; // Caminho do arquivo na mesma pasta do HTML
 
     // Função para buscar o arquivo Excel e processá-lo
     async function fetchAndProcessExcel(url) {
@@ -16,42 +16,47 @@
         const worksheet = workbook.Sheets[guiaBackEnd];
 
         // Lê os valores das células específicas
-        const enderecoAtendidas = 'N4';
-        const enderecoAtendidasN = 'N5';
+        const endAtendidas = 'N4';
+        const endAtendidasN = 'N5';
 
-        const enderecoOrigemNome1 = 'P4'
-        const enderecoOrigemValor1 = 'Q4'
-        const enderecoOrigemNome2 = 'P5'
-        const enderecoOrigemValor2 = 'Q5'
-        const enderecoOrigemNome3 = 'P6'
-        const enderecoOrigemValor3 = 'Q6'
-        const enderecoOrigemNome4 = 'P7'
-        const enderecoOrigemValor4 = 'Q7'
+        const endOrigemN1 = 'P4'
+        const endOrigemVal1 = 'Q4'
+        const endOrigemN2 = 'P5'
+        const endOrigemVal2 = 'Q5'
+        const endOrigemN3 = 'P6'
+        const endOrigemVal3 = 'Q6'
+        const endOrigemN4 = 'P7'
+        const endOrigemVal4 = 'Q7'
 
-        const enderecoEmpreAtend = 'T4'
-        const enderecoEmpreAtendN = 'T5'
-        const enderecoSemLigacao = 'T6'
+        const endEmpresaAtend = 'T4'
+        const endEmpresaAtendN = 'T5'
+        const endSemLig = 'T6'
+        
+        // ----------------------------------------------------------------------------------- //
+        
+        const valorAtendidas = worksheet[endAtendidas] ? worksheet[endAtendidas].v : null;
+        const valorAtendidasN = worksheet[endAtendidasN] ? worksheet[endAtendidasN].v : null;
+        
+        const nomeOrigem1 = worksheet[endOrigemN1] ? worksheet[endOrigemN1].v : null;
+        const valorOrigem1 = worksheet[endOrigemVal1] ? worksheet[endOrigemVal1].v : null;
+        const nomeOrigem2 = worksheet[endOrigemN2] ? worksheet[endOrigemN2].v : null;
+        const valorOrigem2 = worksheet[endOrigemVal2] ? worksheet[endOrigemVal2].v : null;
+        const nomeOrigem3 = worksheet[endOrigemN3] ? worksheet[endOrigemN3].v : null;
+        const valorOrigem3 = worksheet[endOrigemVal3] ? worksheet[endOrigemVal3].v : null;
+        const nomeOrigem4 = worksheet[endOrigemN4] ? worksheet[endOrigemN4].v : null;
+        const valorOrigem4 = worksheet[endOrigemVal4] ? worksheet[endOrigemVal4].v : null;
+
+        const valorEmpresaAtend = worksheet[endEmpresaAtend] ? worksheet[endEmpresaAtend].v : null;
+        const valorEmpresaAtendN = worksheet[endEmpresaAtendN] ? worksheet[endEmpresaAtendN].v : null;
+        const valorSemLig = worksheet[endSemLig] ? worksheet[endSemLig].v : null;
         
 
-        
-        const valorAtendidas = worksheet[enderecoAtendidas] ? worksheet[enderecoAtendidas].v : null;
-        const valorAtendidasN = worksheet[enderecoAtendidasN] ? worksheet[enderecoAtendidasN].v : null;
-        
-        const valorPriscylla = worksheet[enderecoPriscylla] ? worksheet[enderecoPriscylla].v : null;
-        const valorMauro = worksheet[enderecoMauro] ? worksheet[enderecoMauro].v : null;
-        const valorCedente = worksheet[enderecoCedente] ? worksheet[enderecoCedente].v : null;
-        const valorAlex = worksheet[enderecoAlex] ? worksheet[enderecoAlex].v : null;
-
-        return { valorExterno, valorInterno, valorPriscylla, valorMauro, valorCedente, valorAlex };
+        return { valorAtendidas, valorAtendidasN, nomeOrigem1, nomeOrigem2, nomeOrigem3, nomeOrigem4, valorOrigem1, valorOrigem2, valorOrigem3, valorOrigem4, valorEmpresaAtend, valorEmpresaAtendN, valorSemLig};
     }
 
     try {
         // Processa o arquivo Excel
-        const { valorExterno, valorInterno, valorPriscylla, valorMauro, valorCedente, valorAlex } = await fetchAndProcessExcel(fileUrl);
-
-        // Exibe os valores no console (opcional)
-        console.log('Valor Externo:', valorExterno);
-        console.log('Valor Interno:', valorInterno);
+        const { valorAtendidas, valorAtendidasN, nomeOrigem1, nomeOrigem2, nomeOrigem3, nomeOrigem4, valorOrigem1, valorOrigem2, valorOrigem3, valorOrigem4, valorEmpresaAtend, valorEmpresaAtendN, valorSemLig } = await fetchAndProcessExcel(fileUrl);
 
         // Atualiza o gráfico com os valores do Excel
         const chart1 = document.getElementById('grafico1');
@@ -62,11 +67,11 @@
         new Chart(chart1, {
             type: 'pie',
             data: {
-                labels: ['Externo', 'Interno'],
+                labels: ['Atendidas', 'Não atendidas'],
                 datasets: [
                     {
                         label: 'Status',
-                        data: [valorExterno, valorInterno], // Usa os valores extraídos
+                        data: [valorAtendidas, valorAtendidasN], // Usa os valores extraídos
                         borderWidth: 1,
                         hoverOffset: 10,
                         backgroundColor: ['#FF6384', '#36A2EB'], // Cores personalizadas
@@ -91,11 +96,11 @@
         new Chart(chart2, {
             type: 'pie',
             data: {
-                labels: ['PRISCYLLA', 'MAURO', 'ALEX', 'VISÃO CEDENTE'],
+                labels: [nomeOrigem1, nomeOrigem2, nomeOrigem3, nomeOrigem4],
                 datasets: [
                     {
                         label: 'Status',
-                        data: [valorPriscylla, valorMauro, valorAlex, valorCedente],
+                        data: [valorOrigem1, valorOrigem2, valorOrigem3, valorOrigem4],
                         borderWidth: 1,
                         hoverOffset: 10
                     }
@@ -119,11 +124,11 @@
         new Chart(chart3, {
             type: 'pie',
             data: {
-                labels: ['CADASTRO', 'CALL', 'COMITE', 'CONTRATO', 'PÓS CALL', 'PRÉ-ANALISE'],
+                labels: ['Empresas Atendidas', 'Empresas não atendidas', 'Sem ligação'],
                 datasets: [
                     {
                         label: 'Status',
-                        data: [1, 8, 3, 1, 8, 4],
+                        data: [valorEmpresaAtend, valorEmpresaAtendN, valorSemLig],
                         borderWidth: 1,
                         hoverOffset: 10
                     }
@@ -138,38 +143,6 @@
                             boxWidth: 20,
                             boxHeight: 20,
                             borderRadius: 5,
-                        }
-                    },
-                }
-            }
-        })
-
-        new Chart(chart4, {
-            type: 'pie',
-            data: {
-                labels: ['Primeira operação', 'Formalização do contrato', 'Comitê', 'Visita', 'Cadastro', 'Contato', 'Envio de documentação', 'Pós-call', 'Envio de proposta'],
-                datasets: [
-                    {
-                        label: 'Status',
-                        data: [27, 15, 7, 4, 3, 3, 3, 3, 3,],
-                        borderWidth: 1,
-                        hoverOffset: 10
-                    }
-                ]
-            },
-            options: {
-                resposive: true,
-                plugins: {
-                    legend: {
-                        position: 'bottom',
-                        labels: {
-                            boxWidth: 20,
-                            boxHeight: 20,
-                            borderRadius: 5,
-                            padding: 4,
-                            font: {
-                                size: 12
-                            }
                         }
                     },
                 }
